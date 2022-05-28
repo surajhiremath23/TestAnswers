2ab24055763efb35c3e6896184e4a33d5bd1cb40
Suraj Hiremath <71200662+surajhiremath23@users.noreply.github.com>
2022-05-28 20:30:07 +0530

    Update incrementDigit.py

--- incrementDigit.py
+++ incrementDigit.py
@@ -1,9 +1,10 @@
 num =  input()
+num = list(num)
 for i in range(len(num)):
     if int(num[i]) == 9:
-       num=num.replace(num[i],'0')
+       num[i]='0'
     else:
         temp=int(num[i]) + 1
-        num=num.replace(num[i],str(temp))
-        
-print(num)
+        num[i]=temp
+
+print(*num)
